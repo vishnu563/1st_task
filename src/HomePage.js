aed4041f18d503f3907d2415dcc19704eb2bb8de
vishnu563 <vishnuprasanth050603@gmail.com>
2024-07-27 23:23:31 +0530

    center container added

--- src/HomePage.js
+++ src/HomePage.js
@@ -18,62 +18,38 @@ function HomePage() {
                     <div className='validemails'>
                         <p className='vm'>Valid Email(s)</p>
                         <div className='validscroll'>
-                        <text>color	string	whitesmodkbdskjbfkskjfnjl</text><br/>
-                        <text>color	string	whitesmo</text><br/>
-                        <text>color	string	whitesmo</text><br/>
-                        <text>color	string	whitesmo</text><br/>
-                        <text>color	string	whitesmo</text><br/>
-                        <text>color	string	whitesmo</text><br/>
-                        <text>color	string	whitesmo</text><br/>
-                        <text>color	string	whitesmo</text><br/>
-                        <text>color	string	whitesmo</text><br/>
-                        <text>color	string	whitesmo</text><br/>
-                        <text>color	string	whxz,m c C jsCNnNcitesmo</text><br/>
-                        <text>color	string	whitesmo</text><br/>
-                        <text>color	string	whitesmo</text><br/>
-                        <text>color	string	whitesmo</text><br/>
-                        <text>color	string	whitesmo</text><br/>
-                        <text>color	string	white,mzx x l smo</text><br/>
-                        <text>color	string	whitesmo</text><br/>
-                        <text>color	string	whitesmo</text><br/>
-                        <text>color	string	whitesmo</text><br/>
-                        <text>color	string	whitesmo</text><br/>
-                        <text>color	string	whitesmo</text><br/>
-                        <text>color	string	whitesmo</text><br/>
-                        <text>color	string	whitesmo</text><br/>
-                        <text>color	string	whitesmo</text><br/>
-                        <text>color	string	whitesmo</text><br/>
-                        <text>color	string	whitesmo</text><br/>
-                        <text>color	string	whitesmo</text><br/>
-                        <text>color	string	whitesmo</text><br/>
-                        <text>color	string	whitesmo</text><br/>
-                        <text>color	string	whitesmo</text><br/>
-                        <text>color	string	whitesmo</text><br/>
-                        <text>color	string	whitesmo</text><br/>
-                        <text>color	string	whitesmo</text><br/>
+                        <text>color	string</text><br/>
                         </div>
                     </div>
                         <div className='sendcontainer'>
-                        <div className='sendbtn'>
-                        <button className='send'>
-                            send
-                        </button>
-                        <div className='icon'>
-                        <FontAwesomeIcon icon="fa-regular fa-envelope" />
+                            <div className='sendbtn'>
+                            <button className='send'>
+                                send
+                            </button>
+                            <div className='icon'>
+                            <FontAwesomeIcon icon="fa-regular fa-envelope" />
+                            </div>
+                            </div>
                         </div>
-                        </div>
-                </div>
+        </div>  
+        <div className='centercontainer'>
+            <div>
+                <p className='vm'>Invalid Mail(s)</p>
+            </div>
+            <div className='invalidmail'>
+
+            </div>
         </div>
         <div className='rightside_container'>
             <div className='forms'>
-                <label htmlFor="email">From</label>
+                <label htmlFor="email">sender mail-id</label>
                 <input className='inputs' type="email" id="email" name="email" placeholder="Enter email"></input>
                 <label htmlFor="subject">Subject</label>
                 <input className='inputs' type="text" id="subject" name="subject" placeholder="Enter subject"></input>
-                <label htmlFor="body">Body</label>
+                <label htmlFor="body">content</label>
                 <textarea className='inputs' id="body" name="body" placeholder="Enter body Text"></textarea>
                 <label>Upload .csv File</label>
-                <input className='files' type="file" id='btn'></input>
+                <input className='files' type="file" accept=".csv" id='btn'></input>
                 <div className='upcontainer'>
                     <div className='uploadbtn'>
                         <label htmlFor="btn" className='upload'>
