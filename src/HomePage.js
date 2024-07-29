dfd07ce90b0036d24a4c26efb9f5b313b9200074
vishnu563 <vishnuprasanth050603@gmail.com>
2024-07-29 16:42:25 +0530

    upload and send button fixed

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,36 +1,65 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Papa from 'papaparse';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faEnvelope } from '@fortawesome/free-regular-svg-icons';
 import { faArrowUpFromBracket } from '@fortawesome/free-solid-svg-icons';
-
-
 library.add(faEnvelope,faArrowUpFromBracket);
 
+
+
 function HomePage() {
+    
+    const[data,setData]=useState([]);
+
+    const handleFileUpload = (e) => {
+        const file = e.target.files[0];
+        Papa.parse(file,{
+            header: true,
+            complete: (results) =>{
+                setData(results.data);
+            },
+        });
+    }
+
   return (
   <>
-  <div className='header'>
-    <h2 className='h2'>Mass Mail Dispatcher</h2>
-  </div>
-  <div className='container'>
+    <div className='header'>
+        <h2 className='h2'>Mass Mail Dispatcher</h2>
+    </div>
+    <div className='container'>
         <div className='leftside_container'>
-                    <div className='validemails'>
-                        <p className='vm'>Valid Email(s)</p>
-                        <div className='validscroll'>
-                        <text>vishnuprasanth@gmail.com</text><br/>
-                        </div>
+            <div className='validemails'>
+                <p className='vm'>Valid Email(s)</p>
+                    <div className='validscroll'>
+                        <text>
+                            {data.length ? (
+                            <table>
+                                <tbody>
+                                    {data.map((item, index) => (
+                                    <tr key={index}>
+                                        <td>{item.Email}</td>
+                                    </tr>
+                                    ))}
+                                </tbody>
+                            </table>
+                            ) : null}
+                        </text>
                     </div>
-                        <div className='sendcontainer'>
-                            <div className='sendbtn'>
-                            <button className='send'>
-                                send
-                            </button>
-                            <div className='icon'>
-                            <FontAwesomeIcon icon="fa-regular fa-envelope" />
-                            </div>
+                </div>
+                <label className='uploadlabel' >Upload .csv File</label>
+                    <input className='files' type="file" accept=".csv" onChange={handleFileUpload} id='btn'></input>
+                        <div className='upcontainer'>
+                            <div className='uploadbtn'>
+                                <label htmlFor="btn" className='upload'>
+                                    upload
+                                </label>
+                                <div className='uploadicon'>
+                                    <FontAwesomeIcon icon={faArrowUpFromBracket} />
+                                </div>
                             </div>
                         </div>
+                        
         </div>  
         <div className='centercontainer'>
             <div>
@@ -43,22 +72,20 @@ function HomePage() {
         <div className='rightside_container'>
             <div className='forms'>
                 <label htmlFor="email">sender mail-id</label>
-                <input className='inputs' type="email" id="email" name="email" placeholder="Enter email"></input>
+                    <input className='inputs' type="email" id="email" name="email" placeholder="Enter email"></input>
                 <label htmlFor="subject">Subject</label>
-                <input className='inputs' type="text" id="subject" name="subject" placeholder="Enter subject"></input>
+                    <input className='inputs' type="text" id="subject" name="subject" placeholder="Enter subject"></input>
                 <label htmlFor="body">content</label>
-                <textarea className='inputs' id="body" name="body" placeholder="Enter body Text"></textarea>
-                <label>Upload .csv File</label>
-                <input className='files' type="file" accept=".csv" id='btn'></input>
-                <div className='upcontainer'>
-                    <div className='uploadbtn'>
-                        <label htmlFor="btn" className='upload'>
-                            upload
-                        </label>
-                    <div  className='uploadicon'>
-                        <FontAwesomeIcon icon={faArrowUpFromBracket} />
-                    </div>
-                    </div>
+                    <textarea className='inputs' id="body" name="body" placeholder="Enter body Text"></textarea>
+                <div className='sendcontainer'>
+                            <div className='sendbtn'>
+                                <button className='send'>
+                                    send
+                                </button>
+                            <div className='icon'>
+                            <FontAwesomeIcon icon="fa-regular fa-envelope" />
+                            </div>
+                            </div>
                 </div>
             </div>
         </div>
